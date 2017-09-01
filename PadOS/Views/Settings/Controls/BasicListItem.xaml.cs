d689b986eadd59093c1cc11837b226698af0a674
cyberfoxhax <cyber_fox_2@hotmail.com>
2017-09-01 15:16:38 +0800

    making BasicListItem pass gamepad event to icommand

--- PadOS/Views/Settings/Controls/BasicListItem.xaml.cs
+++ PadOS/Views/Settings/Controls/BasicListItem.xaml.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace PadOS.Views.Settings.Controls{
 	public partial class BasicListItem {
@@ -9,30 +12,33 @@ namespace PadOS.Views.Settings.Controls{
 		public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register(
 			"IsActive", typeof(bool), typeof(BasicListItem), new PropertyMetadata(default(bool)));
 
-		public bool IsActive {
-			get => (bool) GetValue(IsActiveProperty);
+		public bool IsActive
+		{
+			get => (bool)GetValue(IsActiveProperty);
 			set => SetValue(IsActiveProperty, value);
 		}
 
 		public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register(
-			"ImageSource", typeof(System.Windows.Media.ImageSource), typeof(BasicListItem), new PropertyMetadata(default(System.Windows.Media.ImageSource)));
+			"ImageSource", typeof(ImageSource), typeof(BasicListItem), new PropertyMetadata(default(ImageSource)));
 
-		public System.Windows.Media.ImageSource ImageSource {
-			get => (System.Windows.Media.ImageSource)GetValue(ImageSourceProperty);
+		public ImageSource ImageSource
+		{
+			get => (ImageSource)GetValue(ImageSourceProperty);
 			set => SetValue(ImageSourceProperty, value);
 		}
 
 		public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
 			"Text", typeof(string), typeof(BasicListItem), new PropertyMetadata("null"));
 
-		public string Text {
+		public string Text
+		{
 			get => (string)GetValue(TextProperty);
 			set => SetValue(TextProperty, value);
 		}
 
-		public System.Windows.Input.ICommand Click { get; set; }
+		public ICommand Click { get; set; }
 
-		public virtual void OnClick() {
+		public virtual void OnClick(object sender, EventArgs args) {
 			Click?.Execute(this);
 		}
 	}
